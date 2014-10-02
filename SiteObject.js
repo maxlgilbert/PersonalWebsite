74f6c669e3d46a756a76d82bb8f207b06907f3c5
Max Gilbert <gilbertlmax@gmail.com>
2014-10-01 20:48:36 -0400

    no canvas text :(

--- SiteObject.js
+++ SiteObject.js
@@ -26,26 +26,6 @@ GAME.SiteObject = function( params ) {
                           this.siteObject.bounds.top+params.y,
                           this.siteObject.position.z + params.z );
     }
-        // create a canvas element
-    /*if (params.)
-    var canvas1 = document.createElement('canvas');
-    var context1 = canvas1.getContext('2d');
-    context1.font = "Bold 40px Arial";
-    context1.fillStyle = "rgba(255,0,0,0.95)";
-    context1.fillText('Hello, world!', 0, 50);
-    
-    // canvas contents will be used for a texture
-    var texture1 = new THREE.Texture(canvas1) 
-    texture1.needsUpdate = true;
-      
-    var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
-    material1.transparent = true;
-
-    var mesh1 = new THREE.Mesh(
-        new THREE.PlaneGeometry(canvas1.width, canvas1.height),
-        material1
-      );*/
-
     if(this.width != 0){
     	var material = new THREE.MeshPhongMaterial({color: params.color, transparent:true});
     	if (params.map) {
